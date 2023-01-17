9bc9df76af0435a353bf6c6d1981fe87f0b885a7
rodminjo <rodminjo@naver.com>
2023-01-17 21:16:56 +0900

    230117 programers coding test lv1 Practice

---
+++ src/lv1/donthavenumberplus/Solution.java
@@ -0,0 +1,11 @@
+package lv1.donthavenumberplus;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum =45;
+        for(int i : numbers){
+            sum -= i;
+        }
+        return sum;
+    }
+}
---
+++ src/lv1/donthavenumberplus/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv1.donthavenumberplus;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{1,2,3,4,6,7,8,0}));
+    }
+}
