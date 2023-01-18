ff978512167a8ab2c55ad2ce0da425f6192c919e
rodminjo <rodminjo@naver.com>
2023-01-18 17:41:29 +0900

    230117 programers coding test lv1 Practice

---
+++ src/lv1/middlechatget/Solution.java
@@ -0,0 +1,13 @@
+package lv1.middlechatget;
+
+class Solution {
+    public String solution(String s) {
+        String[] str =s.split("");
+        int leng = s.length();
+        if (leng % 2 == 0){
+            return str[(leng / 2)-1] + str[(leng / 2)];
+        } else{
+            return str[(leng / 2)];
+        }
+    }
+}
