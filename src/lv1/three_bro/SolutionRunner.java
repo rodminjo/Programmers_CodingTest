c6471a4021d7423320edc4945ea698728774dcaa
rodminjo <rodminjo@naver.com>
2023-01-25 19:30:07 +0900

    230125 programers coding test lv1 Practice

---
+++ src/lv1/three_bro/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv1.three_bro;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{-2,3,0,2,-5}));
+    }
+}
