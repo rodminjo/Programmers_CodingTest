be2fd9488483c37de130ae451899fdbdca13c23e
rodminjo <rodminjo@naver.com>
2023-01-28 22:37:49 +0900

    230128 programers coding test lv1 Practice

--- src/lv1/year2016/Solution.java
+++ src/lv1/year2016/Solution.java
@@ -5,44 +5,54 @@ import java.util.List;
 
 class Solution {
     public String solution(int a, int b) {
-//        int days = daySum(a) + b;
-//        switch(days%7){
-//            case 1 : return "FRI";
-//            case 2 : return "SAT";
-//            case 3 : return "SUN";
-//            case 4 : return "MON";
-//            case 5 : return "TUE";
-//            case 6 : return "WEN";
-//        }
         LocalDate date = LocalDate.of(2016, a, b);
-        String day = date.getDayOfWeek().toString().substring(0,3);
+        String day = date.getDayOfWeek().toString().substring(0, 3);
         return day;
+
     }
 
-//    int daySum(int a){
-//        List<Integer> thirtyoneArr = List.of(1,3,5,7,8,10,12);
-//        List<Integer> thirtyArr = List.of(4,6,9,11);
-//        List<Integer> twentynineArr =List.of(2);
-//        int sum =0;
-//        for(int i : thirtyoneArr){
-//            if(i < a){
-//                sum += 31;
-//            } else{
-//                break;
-//            }
-//        }
-//        for(int i : thirtyArr){
-//            if(i < a){
-//                sum += 30;
-//            } else{
-//                break;
-//            }
-//        }
-//
-//        if ( 2 < a){
-//            sum += 29;
-//        }
-//
-//        return sum;
-//    };
+    public String solution2 ( int a, int b) {
+            List<Integer> thirtyoneArr = List.of(1, 3, 5, 7, 8, 10, 12);
+            List<Integer> thirtyArr = List.of(4,6,9,11);
+            List<Integer> twentynineArr =List.of(2);
+            String day = "THU";
+            int sum =0;
+            for(int i : thirtyoneArr){
+                if(i < a){
+                    sum += 31;
+                } else{
+                    break;
+                }
+            }
+            for(int i : thirtyArr){
+                if(i < a){
+                    sum += 30;
+                } else{
+                    break;
+                }
+            }
+
+            if (2 < a){
+                sum += 29;
+            }
+
+            sum += b;
+
+            switch(sum%7){
+                case 1 : day =  "FRI";
+                break;
+                case 2 : day =  "SAT";
+                break;
+                case 3 : day = "SUN";
+                break;
+                case 4 : day = "MON";
+                break;
+                case 5 : day = "TUE";
+                break;
+                case 6 : day = "WED";
+            }
+
+            return day;
+    };
+
 }
--- src/lv1/year2016/SolutionRunner.java
+++ src/lv1/year2016/SolutionRunner.java
@@ -3,6 +3,16 @@ package lv1.year2016;
 public class SolutionRunner {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution(8,31));
+        System.out.println(sol.solution(1,6));
+        System.out.println(sol.solution2(1,6));
+        for (int i =1 ; i<= 12 ; i++){
+            for (int j =1 ; j<= 29 ; j++){
+                if(sol.solution(i,j).equals(sol.solution2(i,j))){
+                }else{
+                    System.out.println(i+"M"+j+"d");
+                }
+            }
+        }
+
     }
 }
