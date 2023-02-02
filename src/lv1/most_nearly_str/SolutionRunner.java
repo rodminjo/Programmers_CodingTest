a3e6b31048ebc70b071e08a3820875fe38f08b3e
rodminjo <rodminjo@naver.com>
2023-02-02 11:15:14 +0900

    230202 programers coding test lv1 Practice

---
+++ src/lv1/most_nearly_str/SolutionRunner.java
@@ -0,0 +1,10 @@
+package lv1.most_nearly_str;
+
+import java.util.Arrays;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution("foobar")));
+    }
+}
