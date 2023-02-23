075bfcb33d3128794274643b4b29f89d6edaa9fb
rodminjo <rodminjo@naver.com>
2023-02-23 09:25:20 +0900

    230223 programers coding test lv2 Practice

---
+++ src/lv2/Making_min/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv2.Making_min;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{1,4,2},new int[]{5,4,4}));
+    }
+}
