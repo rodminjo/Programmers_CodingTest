41109dacae7e64aa40d6fb117911fe536c43b723
rodminjo <rodminjo@naver.com>
2023-03-14 09:54:48 +0900

    230314 programers coding test lv2 Practice

---
+++ src/lv2/citrus_choice/Solution.java
@@ -0,0 +1,39 @@
+package lv2.citrus_choice;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int compareQuantity = 0;
+
+        for (int i : tangerine){
+           int count=0;
+           if (map.containsKey(i)){
+               count = map.get(i);
+           }
+           map.put(i, ++count);
+        }
+
+        List<Map.Entry<Integer, Integer>> listEntry = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
+
+        Collections.sort(listEntry, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : listEntry){
+            answer ++;
+            compareQuantity += entry.getValue();
+            if (compareQuantity>=k){
+                break;
+            }
+        }
+
+        map.keySet()
+        return answer;
+    }
+}
---
+++ src/lv2/citrus_choice/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv2.citrus_choice;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(6,new int[]{1, 3, 2, 5, 4, 5, 2, 3}));
+    }
+}
