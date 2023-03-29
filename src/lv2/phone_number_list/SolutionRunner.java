628175378c11555207b555af3dabe944bcc89413
rodminjo <rodminjo@naver.com>
2023-03-29 10:34:02 +0900

    230329 programers coding test lv2 Practice

---
+++ src/lv2/phone_number_list/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv2.phone_number_list;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new String[]{"119", "97674223", "1195524421"}));
+    }
+}
