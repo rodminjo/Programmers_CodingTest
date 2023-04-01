cd65c5c4dd5156dc5da963dc10eea41e3763b5ea
rodminjo <rodminjo@naver.com>
2023-04-01 23:21:24 +0900

    230401 programers coding test lv0 Practice

---
+++ src/lv0/finding_location_of_spot/Solution.java
@@ -0,0 +1,22 @@
+package lv0.finding_location_of_spot;
+
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 0;
+        if(dot[0]<0){
+            if(dot[1]<0){
+                answer = 3;
+            } else {
+                answer = 2;
+            }
+        } else if(dot[0]>0){
+            if(dot[1]<0){
+                answer = 4;
+            } else {
+                answer = 1;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ src/lv0/finding_location_of_spot/SolutionRunner.java
@@ -0,0 +1,8 @@
+package lv0.finding_location_of_spot;
+
+public class SolutionRunner {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{2,4}));
+    }
+}
